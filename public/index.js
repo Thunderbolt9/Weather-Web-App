4ee2bc3b8f233df1d6cf1567934d002174bae249
Bhavesh Yadav <bhaveshyadav@Bhaveshs-MacBook-Air.local>
2021-10-01 23:36:32 +0530

    Get latitude and longitude  data and call API for weather reports from the server

---
+++ public/index.js
@@ -0,0 +1,41 @@
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(getWeatherData);
+
+} else {
+  alert("Can't access the location, Please turn on GPS")
+}
+
+
+
+async function getWeatherData(position) {
+  const lat = position.coords.latitude;
+  const long = position.coords.longitude;
+  const location = { lat, long };
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(location),
+    headers: {
+      "Content-Type": "application/json",
+      'Accept': 'application/json'
+    }
+  };
+
+  // Client request for weather data to server
+  const response = await fetch('/', options);
+  const result = await response.json();
+
+  showWeatherData(result);
+};
+
+const tz = document.getElementById('location');
+const temp = document.getElementById('temp');
+const desc = document.getElementById('description');
+
+function showWeatherData(result) {
+  tz.innerText = result.data.timezone;
+  temp.innerText = result.data.current.temp.toPrecision(2) + '°';
+  desc.innerText = result.data.current.weather[0].description;
+  console.log(result.data.timezone);
+}
+
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const https = require('https');
+require('dotenv').config();
+
+app.listen(3000, () => {
+  console.log("Listening to 3000");
+});
+
+app.use(express.static('public'));
+app.use(express.json());
+
+
+app.post('/', (req, res) => {
+  const lat = req.body.lat.toFixed(2);
+  const lon = req.body.long.toFixed(2);
+  const api_key = process.env.API_KEY;
+  const api_url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,daily&appid=${api_key}&units=metric`;
+
+  https.get(api_url, (response) => {
+    let data = '';
+
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    response.on('end', () => {
+      res.json({
+        status: "success",
+        data: JSON.parse(data)
+      });
+    });
+
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
+});
